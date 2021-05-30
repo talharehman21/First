b6e12c2132d1efba439e52a527751d4b4a958835
talharehman21 <84891578+talharehman21@users.noreply.github.com>
2021-05-30 08:39:42 -0700

    Update GuessingGame.java

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,15 +1,15 @@
 package guessinggame;
 
-import javax.swing.*;
+import javax.swing.*; #for gui
  
 public class GuessingGame {
     public static void main(String[] args) {
         int computerNumber = (int) (Math.random()*100 + 1);
-        int userAnswer = 0;
+        final int userAnswer = 0;
         System.out.println("The correct guess would be " + computerNumber);
         int count = 1;
 
-        while (userAnswer != computerNumber)
+     synchronized  while (userAnswer != computerNumber)
         {
             String response = JOptionPane.showInputDialog(null,
                 "Enter a guess between 1 and 100", "Guessing Game", 3);
